56ed6c2d0b0c149886a4d1352aeef131a708b0ba
AnsGoo <haiven_123@163.com>
2019-11-06 22:20:58 +0800

    ant-cron

---
+++ README.md
@@ -0,0 +1,41 @@
+# Ant-cron
+
+这是一个cron表达式生成插件,基于vue与Ant-desgin-vue实现
+
+## 依赖
+- Vue 2.0.0+
+- ant-design-vue 1.4.4+
+
+## 使用方式
+```
+<a-form :form="form" @submit="handleSubmit">     
+    <a-form-item
+    :labelCol="labelCol"
+    :wrapperCol="wrapperCol"
+    label="cron表达式">
+    <a-cron ref="innerVueCron" v-decorator="['cronExpression', {'initialValue':'0 0 0 2 * ?',rules: 
+        [{ required: true, message: '请输入cron表达式!' }]}]"  @change="setCorn"></a-cron>
+    </a-form-item>
+</a-form>
+...
+
+
+setCorn(data){
+    this.$nextTick(() => {
+        this.form.setFieldsValue({cronExpression: data})
+    })
+}
+```
+## 参考项目
+
+[vue-cron](https://github.com/1615450788/vue-cron)
+[ant-design-vue 开发的cron表达式组件](https://blog.csdn.net/chizhuo9591/article/details/100732548)
+
+## 改进点
+
+- 解决了部分bug
+    
+## 联系方式
+
+邮箱 : haiven_123@163.com
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,55 @@
+  <template>
+  <div>
+    <a-form :form="form" @submit="handleSubmit">     
+      <a-form-item
+        :labelCol="labelCol"
+        :wrapperCol="wrapperCol"
+        label="cron表达式">
+        <a-cron ref="innerVueCron" v-decorator="['cronExpression', {'initialValue':'0 0 0 2 * ?',rules: 
+          [{ required: true, message: '请输入cron表达式!' }]}]"  @change="setCorn"></a-cron>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+import ACron from "@/components/Acron";
+
+export default {
+  components: { ACron },
+  data () {
+    return {
+      visible: false,
+      formLayout: 'horizontal',
+      form: this.$form.createForm(this, { name: 'coordinated' }),
+      labelCol: {
+          xs: { span: 5 },
+          sm: { span: 5 },
+          md: { span: 5 },
+          lg: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 14 },
+          sm: { span: 14 },
+          md: { span: 14 },
+          lg: { span: 14 },
+        },
+    }
+  },
+  methods: {
+    handleSubmit (e) {
+      e.preventDefault()
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values)
+        }
+      })
+    },
+    setCorn(data){
+      this.$nextTick(() => {
+         this.form.setFieldsValue({cronExpression: data})
+      })
+    }
+  }
+}
+</script>
